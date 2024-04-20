a89004cf64ecd20e88f5e03e667d4f9b0d752941
ImAHappyDuck <126510779+ImAHappyDuck@users.noreply.github.com>
2024-04-20 09:59:04 -0400

    Started working on writing to csv. Needs debugging yet, and a way to handle commas in in the content text.

--- Final Project/redditAPI.py
+++ Final Project/redditAPI.py
@@ -1,25 +1,32 @@
 import praw
 from datetime import datetime
+import csv
 
-# Initialize Reddit API connection
-reddit = praw.Reddit(
-    client_id='o-k8HsUeNmbssImQgRy9Aw',
-    client_secret='LrEr3JNVjowGLxgQajiPBlH4lIAQSg',
-    user_agent='MyAwesomeRedditApp/1.0 by /u/Necessary-Donut-7119')
+with open('wsbData','w') as csvFile:
+    writer = csv.writer(csvFile, delimiter=' ')
 
-# Define subreddit
-subreddit_name = 'wallstreetbets'
-subreddit = reddit.subreddit(subreddit_name)
+    # Initialize Reddit API connection
+    reddit = praw.Reddit(
+        client_id='o-k8HsUeNmbssImQgRy9Aw',
+        client_secret='LrEr3JNVjowGLxgQajiPBlH4lIAQSg',
+        user_agent='MyAwesomeRedditApp/1.0 by /u/Necessary-Donut-7119')
 
-# Get posts from the subreddit
-posts = subreddit.new(limit=1000)
+    # Define subreddit
+    subreddit_name = 'wallstreetbets'
+    subreddit = reddit.subreddit(subreddit_name)
 
-# Iterate over the posts and print some information
-for post in posts:
-    print("Title:", post.title)
-    print("Content Text:", post.selftext)
-    print("Score:", post.score)
-    print("Comments count:", post.num_comments)
-    print("URL:", post.url)
-    print("Date:", datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S'))
-    print("------------------------------")
+    # Get posts from the subreddit
+    posts = subreddit.new(limit=1000)
+
+    header = 'date,title,content,score,numComments'
+    writer.writerow(header)
+
+    # Iterate over the posts and print some information
+    for post in posts:
+        row = f'{ datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S')},{post.title},{post.selftext},{post.score},{post.num_comments}'
+        print(row)
+        try:
+         writer.writerow(row)
+        except(UnicodeEncodeError):
+           print()
+        
